b18f95cb31fe4c75565a57c4d13e361ab1b5d392
Suhail Shergill <suhailshergill@gmail.com>
2011-07-04 02:21:47 +0000

    this is what the pr22061-3.c file should look like. this file is a part of suite which were used to force crashes in earlier versions of gcc

--- tests/gcc-tests/pr22061-3.c
+++ tests/gcc-tests/pr22061-3.c
@@ -1,20 +1,4 @@
-typedef int a;
-
-struct MyStruct1
-{
-  struct MyStruct2* x;
-}
-  ;
-struct MyStruct2
-{
-  struct MyStruct1* x;
-  a y;
-  
-}
-  ;
-
-
-a N = 1;
+int N = 1;
 void foo() {} /* Necessary to trigger the original ICE.  */
 void bar (char a[2][N]) { a[1][0] = N; }
 
@@ -29,50 +13,17 @@ main (void)
 
   struct MyStruct3
   {
-    a* x;
-    a y;
+    int y;
   }
   ;
 
 
   bar (x);
  
-  typedef struct 
-  {
-    
-    int age;
-    
-    char *name;
-    
-    enum 
-      {
-	male, female 
-      }
-      sex;
-    
-  }
-  Person;
-  
-
- typedef struct MyStruct3 MyStruct3_t;
-
  if (N[(char *) x] != N)
    {
-     Person p1;
-     p1.sex = male;
-     p1.name = "Tom";
-     p1.age = 43;
-     Person p2;
-     p2.sex = male;
-     p2.name = "Jerry";
-     p2.age = 32;
      abort ();
    }
- 
-
-  MyStruct3_t s2;
-  s2.y = 3;
-  s2.x = &s2.y;
   
   exit (0);
 }
